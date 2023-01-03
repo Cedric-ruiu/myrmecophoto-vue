d17814053e0086b7ca3e168fcabd8c0d692094f9
Cédric Ruiu <cedric.ruiu@gmail.com>
2023-01-04 00:39:16 +0100

    refactor: rename header footer components

---
+++ components/BaseHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <a href="/">logo</a>
+    <nav>
+      <ul>
+        <li><a href="/specimens">Photos spécimens</a></li>
+        <li>
+          <NuxtLink to="/articles">
+            Articles
+          </NuxtLink>
+        </li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
