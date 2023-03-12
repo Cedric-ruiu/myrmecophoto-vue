3726ce11b5eb9f7fe478800ac7768e817bd97b5a
Cédric Ruiu <cedric.ruiu@gmail.com>
2023-03-12 22:35:57 +0100

    fix(email): better encrypted Email

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
+const { data: emailEncrypted } = await useFetch('/api/getEncryptedEmailContact')
+
+if (emailEncrypted == null || emailEncrypted.value == null)
+  throw createError({ statusCode: 404, statusMessage: 'Api getEncryptedEmailContact Not Found' })
 </script>
 
 <template>
   <footer class="[ o-footer ] h-20 container mx-auto w-full p-5 font-text">
-    <p>Cédric RUIU • Vannes, Brittany, France • <MailTo :email="runtimeConfig.emailContact" /></p>
+    <p>Cédric RUIU • Vannes, Brittany, France • <MailTo :email-encrypted="emailEncrypted?.data" /></p>
   </footer>
 </template>
 
--- components/MailTo.vue
+++ components/MailTo.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
 // This component provides the ability to protect me from spam when I want to display an email.
-// The protection is relatively simple, because this site has a low traffic & robots,
-// but I can complicate the algorithm in the future if necessary.
-
+// The protection is relatively simple for now, because this site has a low traffic & robots,
+// but I'll can enhance the algorithm if necessary.
 const props = defineProps({
-  email: { type: String, required: true },
+  emailEncrypted: { type: String, required: true },
 })
 
-const reverseString = (str: string) => {
-  return str.split('').reverse().join('')
-}
+const mailtoEmailEncrypted = `mailto:${props.emailEncrypted}`
 
-const emailReversed = reverseString(props.email)
-const mailtoEmailReversed = `mailto:${reverseString(props.email)}`
-
-const mailtoHandler = () => {
-  // event.preventDefault() // disable original link html event
-  window.open(`mailto:${reverseString(emailReversed)}`, '_blank')
+const mailtoHandler = (e: Event) => {
+  e.preventDefault() // disable original link html event
+  window.open(`mailto:${decryptEmail(props.emailEncrypted)}`, '_blank')
 }
 </script>
 
 <template>
-  <a :href="mailtoEmailReversed" @click="mailtoHandler">&#x202e;{{ emailReversed }}&#x202c;</a>
+  <a :href="mailtoEmailEncrypted" @click="mailtoHandler">&#x202e;{{ emailEncrypted }}&#x202c;</a>
 </template>
