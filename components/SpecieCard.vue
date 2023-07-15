d1eb53761791fbc6ce0a7900a63daf565fe56ea2
Cédric Ruiu <cedric.ruiu@gmail.com>
2023-07-16 00:21:28 +0200

    feat(taxa): add text-shadow on tiles

--- components/SpecieCard.vue
+++ components/SpecieCard.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 <template>
   <NuxtLink
     :to="`/taxons/${props.taxon.replace(' ', '_').replace('.', '')}`"
-    class="[ m-specieCard ] relative inline-flex flex-col justify-end p-3.5 aspect-video"
+    class="[ m-specieCard ] relative inline-flex flex-col justify-end p-3.5 aspect-video text-white"
   >
     <img
       class="[ m-specieCard__bg ] absolute top-0 left-0 w-full h-full object-cover filtered"
@@ -19,8 +19,8 @@ const props = defineProps({
         .replace('.', '')}.jpg`"
       :alt="taxon"
     />
-    <h3 class="relative text-white text-lg font-medium">{{ taxon }}</h3>
-    <p class="relative text-white text-xs font-normal">
+    <h3 class="relative text-shadow-lg text-lg font-medium">{{ taxon }}</h3>
+    <p class="relative text-shadow-lg text-xs font-normal">
       {{ researcherName }} ({{ yearDiscover }})
     </p>
   </NuxtLink>
