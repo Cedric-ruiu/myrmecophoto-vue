43c3c6d51b3cdb8791582776d8c30dbff4e2d7e4
Cédric Ruiu <cedric.ruiu@gmail.com>
2023-07-18 00:08:00 +0200

    perf: cache fetch api request
    try to fix lose data when navigate with nuxt link

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: species } = await useFetch('/api/getSpecies', { key: 'species' })
+const { data: subfamilies } = await useFetch('/api/getTaxa', { key: 'taxa' })
+const { data: emailEncrypted } = await useFetch(
+  '/api/getEncryptedEmailContact',
+  { key: 'emailEncrypted' }
+)
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
