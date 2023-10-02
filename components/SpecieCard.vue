572a9140555f8d1f1b26d412890c084e654885a8
Cédric Ruiu <cedric.ruiu@gmail.com>
2023-10-03 00:40:43 +0200

    feat(specieCard): align UI with taxons page

--- components/SpecieCard.vue
+++ components/SpecieCard.vue
@@ -10,10 +10,10 @@ const props = defineProps({
 <template>
   <NuxtLink
     :to="`/taxons/${props.taxon.replace(' ', '_').replace('.', '')}`"
-    class="[ m-specieCard ] relative inline-flex flex-col justify-end p-3.5 aspect-video text-white"
+    class="[ m-specieCard ] relative inline-flex flex-col justify-end p-3.5 aspect-video text-white specie-card-border"
   >
     <img
-      class="[ m-specieCard__bg ] absolute top-0 left-0 w-full h-full object-cover filtered"
+      class="[ m-specieCard__bg ] absolute top-0 left-0 w-full h-full object-cover rounded-md filtered"
       :src="`/img/taxonomy/thumbnails/${taxon
         .replace(' ', '-')
         .replace('.', '')}.jpg`"
@@ -37,6 +37,16 @@ const props = defineProps({
     .m-specieCard__bg {
       filter: contrast(1) sepia(0) brightness(0.7);
     }
+  }
+
+  &::before {
+    content: '';
+    padding: 0 0 5px 0;
+    background-image: linear-gradient(#222, #000),
+      linear-gradient(90deg, #e72c27 64.09%, #faa307 94.45%);
+    background-clip: content-box, border-box;
+    z-index: -1;
+    @apply absolute w-full h-full rounded-md -bottom-px left-0;
   }
 }
 </style>
