c6c5a79d9dfd2982ce146cd00d419444486f5ec6
Cédric Ruiu <cedric.ruiu@gmail.com>
2025-02-18 00:14:36 +0100

    feat(favicon): add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,23 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'fr',
       },
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96',
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      meta: [{ name: 'apple-mobile-web-app-title', content: 'Myrmecophoto' }],
     },
   },
 
